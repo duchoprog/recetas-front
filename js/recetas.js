01b359c328ca123b39e0aed7205f24068a3a3ce3
duchoprog <duchoprog@gmai.com>
2023-07-02 22:49:48 -0300

    mejoras varias

--- js/recetas.js
+++ js/recetas.js
@@ -1,7 +1,7 @@
 const createRecipe = async (recipeParams) => {
   let returnable;
-  await fetch("https://recetas-back-pe-a3b73f302958.herokuapp.com/openai-api", {
-    //await fetch("http://localhost:8000/openai-api", {
+  //await fetch("https://recetas-back-pe-a3b73f302958.herokuapp.com/openai-api", {
+  await fetch("http://localhost:8000/openai-api", {
     //await fetch("http://localhost:8000/cohere", {
     method: "POST",
     body: JSON.stringify({
@@ -13,8 +13,8 @@ const createRecipe = async (recipeParams) => {
   })
     .then((response) => response.json())
     .then((data) => {
-      //console.log(data);
-      returnable = data;
+      console.log(data.completion);
+      returnable = data.completion;
     })
     .catch((err) => {
       console.log(err.message);
